6ff964de3fca52ef2664050db7a401df3eac8506
MarisabelC <38087342+MarisabelC@users.noreply.github.com>
2021-10-31 11:24:21 -0700

    dropdown buttons

--- lib/input.dart
+++ lib/input.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-
-
 import 'dart:convert' as convert;
-
 import 'package:http/http.dart' as http;
-
 import 'Location.dart';
-
-
-
+import 'package:custom_searchable_dropdown/custom_searchable_dropdown.dart';
 
 class InputPage extends StatefulWidget {
   InputPage({Key? key, required this.title}) : super(key: key);
@@ -17,7 +11,6 @@ class InputPage extends StatefulWidget {
 
   @override
   _InputPageState createState() => _InputPageState();
-
 }
 
 class _InputPageState extends State<InputPage> {
@@ -27,14 +20,17 @@ class _InputPageState extends State<InputPage> {
   TextEditingController zip = TextEditingController();
   List<String> _states = [];
   List<String> _county = [];
-  String? dropdownCounty ;
-  String? dropdownState ;
+  String? dropdownCounty;
+  String? dropdownState;
+  String? _selectedValue;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     states();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,74 +73,73 @@ class _InputPageState extends State<InputPage> {
                   ),
                 ),
               ),
-              DropdownButton<String>(
-                  value: dropdownState,
-                  hint: Text('Select State'),
-                  icon: const Icon(Icons.arrow_downward),
-                  iconSize: 24,
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropdownState = newValue!;
-                      dropdownCounty = null;
-                      county();
-                    });
-                  },
-                  items: _states
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList()),
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DropdownButton<String>(
-                    value: dropdownCounty,
-                    hint: Text('Select County'),
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    onChanged: dropdownState == null ? null:
-                        (String? newValue) {
+                padding: EdgeInsets.all(8),
+                child: CustomSearchableDropDown(
+                  items: _states,
+                  label: 'Select State',
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  // prefixIcon: Padding(
+                  //   padding: const EdgeInsets.all(0.0),
+                  //   child: Icon(Icons.search),
+                  // ),
+                  dropDownMenuItems: _states?.map((item) {
+                        return item;
+                      })?.toList() ??
+                      [],
+                  onChanged: (value) {
+                    if (value != null) {
                       setState(() {
-                        dropdownCounty = newValue!;
+                        dropdownState = value;
+                        dropdownCounty = null;
+                        county();
                       });
-                    },
-                    items: _county
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList()),
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: CustomSearchableDropDown(
+                  items: _county,
+                  label: 'Select County',
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black26,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  // prefixIcon: Padding(
+                  //   padding: const EdgeInsets.all(0.0),
+                  //   child: Icon(Icons.search),
+                  // ),
+                  dropDownMenuItems: _county?.map((item) {
+                        return item;
+                      })?.toList() ??
+                      [],
+                  onChanged: (value) {
+                    if (value != null) {
+                      dropdownCounty = value;
+                    }
+                  },
+                ),
               ),
               ElevatedButton(
                 // style: style,
                 onPressed: () {
                   // Within the `FirstRoute` widget
 
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Location(
-                          address: Address.text,
-                          county: dropdownCounty!,
-                          state: dropdownState!,
-                          zip_code: zip.text,
-                          business_type: Business.text
-                      )),
-                    );
-
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Location(
+                            address: Address.text,
+                            county: dropdownCounty!,
+                            state: dropdownState!,
+                            zip_code: zip.text,
+                            business_type: Business.text)),
+                  );
                 },
                 child: const Text('Search'),
               ),
@@ -155,13 +150,10 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
-
-
   void states() async {
     // This example uses the Google Books API to search for books about http.
     // https://developers.google.com/books/docs/overview
-    var url =
-    Uri.parse('http://10.0.2.2:5000/States');
+    var url = Uri.parse('http://10.0.2.2:5000/States');
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
@@ -176,12 +168,10 @@ class _InputPageState extends State<InputPage> {
     }
   }
 
-
   void county() async {
     // This example uses the Google Books API to search for books about http.
     // https://developers.google.com/books/docs/overview
-    var url =
-    Uri.parse('http://10.0.2.2:5000/${dropdownState}');
+    var url = Uri.parse('http://10.0.2.2:5000/${dropdownState}');
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
@@ -195,5 +185,4 @@ class _InputPageState extends State<InputPage> {
       print('Request failed with status: ${response.statusCode}.');
     }
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyAppState extends State<MyApp> {
           'https://flutter.io/images/catalog-widget-placeholder.png'),
       backgroundColor: Colors.white,
       loaderColor: Colors.red,
+
     );
   }
 }
